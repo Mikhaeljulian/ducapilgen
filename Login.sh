4fc24963678e50504b222757073db48dd10ac386
Mikhael Julian Ramardi <61940351+Mikhaeljulian@users.noreply.github.com>
2023-03-15 16:36:19 +0700

    Ducapil Gen

---
+++ Login.sh
@@ -0,0 +1,18 @@
+pkg install update
+clear
+pkg install upgrade
+clear
+pkg install git
+clear
+pkg install python
+clear
+pkg install python2
+clear
+python2 Security.py
+clear
+python Restart.py
+clear
+cd induk
+clear
+python surabaya_utara.py
+python jakarta_barat.py
